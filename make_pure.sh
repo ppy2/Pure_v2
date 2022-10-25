b835fdad8d08e2dc9d12569fc78386f963f3accc
ppy2 <33607921+ppy2@users.noreply.github.com>
2022-10-25 17:30:15 +0300

    Update make_pure.sh

--- make_pure.sh
+++ make_pure.sh
@@ -2,9 +2,9 @@
 
 # Custom Buildroot linux image Pure (http://puredsd.ru/).
 
-#git clone https://github.com/buildroot/buildroot.git
+git clone https://github.com/buildroot/buildroot.git
 cd buildroot
-#git checkout 3f0ee529083e972be9893676fab00ac50c2816c3
+git checkout 3f0ee529083e972be9893676fab00ac50c2816c3
 make BR2_EXTERNAL=../ pure_defconfig
 
 make
