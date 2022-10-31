ee675a7d6337423c6e607c54cdc16b35c191bcc8
root <root@buster>
2022-10-31 11:11:38 +0300

    modified:   board/pure/rootfs-overlay/opt/copytoemmc.sh

--- board/pure/rootfs-overlay/opt/copytoemmc.sh
+++ board/pure/rootfs-overlay/opt/copytoemmc.sh
@@ -4,9 +4,10 @@
 
 export DISK=/dev/mmcblk1
 dd if=/dev/zero of=${DISK} bs=1M count=10
+sync ; sleep 1
 dd if=/opt/backup/MLO of=${DISK} count=1 seek=1 bs=128k
 dd if=/opt/backup/u-boot.img of=${DISK} count=2 seek=1 bs=384k
-
+sync ; sleep 1
 fdisk -u ${DISK} <<EOF
 n
 p
@@ -15,14 +16,13 @@ p
 +512M
 wq
 EOF
-sleep 1
-partprobe ; sync
-sleep 1
-mkfs.ext4 -F -L rootfs -O ^metadata_csum,^64bit ${DISK}p1 ;sleep 1
-sleep 1
+
+partprobe
+sync ; sleep 1
+mkfs.ext4 -F -L rootfs -O ^metadata_csum,^64bit ${DISK}p1
+sync ; sleep 1
 mount ${DISK}p1 /mnt
-sleep 1
-rsync -a --numeric-ids  --exclude='/proc' --exclude='/sys' --exclude='/mnt'  / /mnt/
+rsync -av --numeric-ids  --exclude='/proc' --exclude='/sys' --exclude='/mnt'  / /mnt/
 mkdir /mnt/proc /mnt/sys /mnt/boot/dtbs /mnt/mnt
 mv /mnt/boot/*.dtb  /mnt/boot/dtbs/
 rm /mnt/boot/MLO /mnt/boot/u-boot.img
@@ -30,9 +30,6 @@ rm /mnt/boot/MLO /mnt/boot/u-boot.img
 cp /opt/backup/uEnv.txt /mnt/boot/
 echo "" >> /mnt/boot/uEnv.txt
 grep "optargs="  /boot/uEnv.txt  >>  /mnt/boot/uEnv.txt
-sync ; umount /mnt
-sleep 5
+
+sync
 poweroff
-#######################################################
-
-
