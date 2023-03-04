ff6ca2aea4f1727fc42bd8dda677697c63de2cfc
root <pavel.pogodin@gmail.com>
2023-03-04 22:37:54 +0300

    modified:   board/pure/rootfs-overlay/etc/rc.pure/S95spotify

--- package/librespot/librespot.mk
+++ package/librespot/librespot.mk
@@ -7,7 +7,7 @@
 LIBRESPOT_VERSION = 0.4.2
 LIBRESPOT_SOURCE = v$(LIBRESPOT_VERSION).tar.gz
 LIBRESPOT_SITE = https://github.com/librespot-org/librespot/archive/refs/tags
-LIBRESPOT_CARGO_BUILD_OPTS = --no-default-features --features "alsa-backend"
+LIBRESPOT_CARGO_BUILD_OPTS =  --no-default-features --features "alsa-backend"
 
 
 $(eval $(cargo-package))
