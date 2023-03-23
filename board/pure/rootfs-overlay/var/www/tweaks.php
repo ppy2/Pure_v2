30f48ec9a801cd556b9fc412f9d8f20dc31346c2
Pavel Pogodin <pavel.pogodin@gmail.com>
2023-03-23 15:33:16 +0300

    Update tweaks.php

--- board/pure/rootfs-overlay/var/www/tweaks.php
+++ board/pure/rootfs-overlay/var/www/tweaks.php
@@ -3,6 +3,7 @@
 <title>Pure Tweaks</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Cache-Control" content="no-cache" />
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link type="text/css" rel="stylesheet" href="style.css">
 <script src="javascript.js"></script>
